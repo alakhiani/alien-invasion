ee24cf0f801328e539e13152eaa931b2e39a86cc
Avinash Lakhiani <avinash.lakhiani@gmail.com>
2023-03-25 15:48:06 -0700

    Added Jaden's rocket ship

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -10,7 +10,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/rocket_ship.png')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
