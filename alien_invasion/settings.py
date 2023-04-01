e7e816755a49688fda876aba389537a78a320761
Avinash Lakhiani <avinash.lakhiani@gmail.com>
2023-04-01 14:28:59 -0700

    Created a row of aliens added to a sprite group

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -10,10 +10,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
         
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 3.5
         
         # Bullets settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
